6168b1971f64a3181b4597fc484a102c4fd5f9a6
Madhavam Shahi <madhavampshahi@gmail.com>
2022-08-20 20:18:02 -0500

    creating google sigin in page

--- edbuddy/lib/views/login/login.dart
+++ edbuddy/lib/views/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginView extends StatefulWidget {
   LoginView({Key? key}) : super(key: key);
@@ -11,16 +12,58 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/illus6.png"),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
               ),
-            ),
-          )
-        ],
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/illus6.png"),
+                  ),
+                ),
+              ),
+              Text(
+                "Login to continue",
+                style: GoogleFonts.josefinSans(
+                  textStyle: TextStyle(
+                    fontSize: 32,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Ink(
+                  color: Color(0xFF397AF3),
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/google_logo.png',
+                          height: 50,
+                        ),
+                        SizedBox(width: 12),
+                        Text(
+                          'Sign in with Google',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
